f313f77090f086f51735f998b74bd14de6f17522
MiguelAGuzman <miguel.a.guzman0009@gmail.com>
2022-11-05 23:40:16 -0400

    profile img styling

--- src/components/ProfileCard/ProfileCard.jsx
+++ src/components/ProfileCard/ProfileCard.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Cover from '../../img/cover.jpg'
 import Profile from '../../img/profileImg.jpg'
+import './ProfileCard.css'
 
 const ProfileCard = () => {
   return (
