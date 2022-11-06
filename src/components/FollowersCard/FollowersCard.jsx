160ac94ed2b1b6eb8681697c95d466a8938ddcab
MiguelAGuzman <miguel.a.guzman0009@gmail.com>
2022-11-06 18:41:19 -0500

    added followers data

--- src/components/FollowersCard/FollowersCard.jsx
+++ src/components/FollowersCard/FollowersCard.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import './FollowersCard.css'
+import { Followers } from '../../Data/FollowersData'
 
 const FollowersCard = () => {
   return (
     <div className="FollowersCard">
         <h3>Who is following you</h3>
+        {Followers .map((follower, id)=>{
+            return(
+                <div className="follower">Yes Follower</div>
+            )
+        })}
     </div>
   )
 }
