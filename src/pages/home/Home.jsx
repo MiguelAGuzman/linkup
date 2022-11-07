3aca290578110a50df0a41fc125839c3a68dddf6
MiguelAGuzman <miguel.a.guzman0009@gmail.com>
2022-11-07 12:13:12 -0500

    added and imported post side files

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ProfileSide from '../../components/profileSide/ProfileSide'
+import PostSide from '../../components/PostSide/ProfileSide'
 import './Home.css'
 const Home = () => {
   return (
     <div className="Home">
         <ProfileSide/>
-        <div className="postSide">Posts</div>
+        <PostSide/>
         <div className="rightSide">Rightside</div>
 
     </div>
