ca689d9f737d014b2a43e05ce0fbe893566332dd
MiguelAGuzman <miguel.a.guzman0009@gmail.com>
2022-11-07 12:14:27 -0500

    fixed import issue

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ProfileSide from '../../components/profileSide/ProfileSide'
-import PostSide from '../../components/PostSide/ProfileSide'
+import PostSide from '../../components/PostSide/PostSide'
 import './Home.css'
 const Home = () => {
   return (
