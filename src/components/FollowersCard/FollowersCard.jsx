299f481af1db31628ae0cc09ba9d57fa848e47c2
MiguelAGuzman <miguel.a.guzman0009@gmail.com>
2022-11-06 19:54:12 -0500

    follwers card adjustments

--- src/components/FollowersCard/FollowersCard.jsx
+++ src/components/FollowersCard/FollowersCard.jsx
@@ -17,7 +17,7 @@ const FollowersCard = () => {
                             <span>@{follower.username}</span>
                         </div>
                     </div>
-                    <button>Follow</button>
+                    <button className='button fc-button'>Follow</button>
                 </div>
             )
         })}
