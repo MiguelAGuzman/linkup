444197c98503be633fcfcfe60143f41b4566d904
MiguelAGuzman <miguel.a.guzman0009@gmail.com>
2022-11-07 23:19:23 -0500

    styled post share and image

--- src/components/PostShare/PostShare.jsx
+++ src/components/PostShare/PostShare.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import ProfileImage from '../../img/profileImg.jpg'
 import './PostShare.css'
+import { UilScenery } from "@iconscout/react-unicons";
 
 const PostShare = () => {
   return (
     <div className="PostShare">
         <img src={ProfileImage} alt=""/>
+        <div>
+            <input type="text" placeholder="What's Happening?"/>
+        </div>
+        <div className="postOptions">
+
+        </div>
     </div>
   )
 }
