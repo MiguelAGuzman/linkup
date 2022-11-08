386a5179ad92b7e15f75c214a486dcbf934f0c56
MiguelAGuzman <miguel.a.guzman0009@gmail.com>
2022-11-07 21:53:48 -0500

    created post share section

--- src/components/PostSide/PostSide.jsx
+++ src/components/PostSide/PostSide.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import PostShare from '../PostShare/PostShare'
 import './PostSide.css'
 
 const PostSide = () => {
   return (
     <div className="PostSide">
-      
-    </div>  )
+      <PostShare/>
+    </div>  
+    )
 }
 
 export default PostSide
