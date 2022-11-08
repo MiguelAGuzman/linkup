87370ab9de15f5accaed009fb0301f81867e0475
MiguelAGuzman <miguel.a.guzman0009@gmail.com>
2022-11-07 23:49:32 -0500

    share options positioning

--- src/components/PostShare/PostShare.jsx
+++ src/components/PostShare/PostShare.jsx
@@ -13,23 +13,23 @@ const PostShare = () => {
         <img src={ProfileImage} alt=""/>
         <div>
             <input type="text" placeholder="What's Happening?"/>
-        </div>
-        <div className="postOptions">
-            <div className="option">
-                <UilScenery/>
-                Photo
-            </div>
-            <div className="option">
-                <UilPlayCircle/>
-                Video
-            </div>
-            <div className="option">
-                <UilLocationPoint/>
-                Location
-            </div>
-            <div className="option">
-                <UilSchedule/>
-                Schedule
+            <div className="postOptions">
+                <div className="option">
+                    <UilScenery/>
+                    Photo
+                </div>
+                <div className="option">
+                    <UilPlayCircle/>
+                    Video
+                </div>
+                <div className="option">
+                    <UilLocationPoint/>
+                    Location
+                </div>
+                <div className="option">
+                    <UilSchedule/>
+                    Schedule
+                </div>
             </div>
         </div>
     </div>
