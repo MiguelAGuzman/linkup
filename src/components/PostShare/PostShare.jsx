5f45ab7f43f8571360a8c92cfd77517bcb350aae
MiguelAGuzman <miguel.a.guzman0009@gmail.com>
2022-11-07 23:05:19 -0500

    added profile image to post share

--- src/components/PostShare/PostShare.jsx
+++ src/components/PostShare/PostShare.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import ProfileImage from '../../img/profileImg.jpg'
 
 const PostShare = () => {
   return (
     <div className="PostShare">
-        
+        <img src={ProfileImage} alt=""/>
     </div>
   )
 }
