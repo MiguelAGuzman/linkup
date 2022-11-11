e17c71f3377eb746c539da3f5a2e79a0a5c93d8b
MiguelAGuzman <miguel.a.guzman0009@gmail.com>
2022-11-11 15:24:06 -0500

    created info card

---
+++ src/components/InfoCard/InfoCard.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './InfoCard.css'
+import {UilPen} from '@iconscout/react-unicons'
+
+const InfoCard = () => {
+  return (
+    <div className="InfoCard">
+        <div className="infoHead">
+            <h4>Tour Info</h4>
+            <UilPen/>
+        </div>
+    </div>
+  )
+}
+
+export default InfoCard
--- src/components/ProfileLeft/ProfileLeft.jsx
+++ src/components/ProfileLeft/ProfileLeft.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import FollowersCard from '../FollowersCard/FollowersCard'
+import InfoCard from '../InfoCard/InfoCard'
+import LogoSearch from '../LogoSearch/LogoSearch'
 import './ProfileLeft.css'
 
 const ProfileLeft = () => {
   return (
     <div className="ProfileLeft">
-        a
+        <LogoSearch/>
+        <InfoCard/>
+        <FollowersCard/>
     </div>
   )
 }
