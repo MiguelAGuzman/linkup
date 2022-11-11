72e9bbec495d657a6b4b5a82196826ca009797d2
MiguelAGuzman <miguel.a.guzman0009@gmail.com>
2022-11-11 15:45:21 -0500

    created info card fields

--- src/components/InfoCard/InfoCard.jsx
+++ src/components/InfoCard/InfoCard.jsx
@@ -9,6 +9,29 @@ const InfoCard = () => {
             <h4>Tour Info</h4>
             <UilPen/>
         </div>
+
+        <div className="info">
+            <span>
+                <b>Status</b>
+            </span>
+            <span>in Relationship</span>
+        </div>
+
+        <div className="info">
+            <span>
+                <b>Lives in</b>
+            </span>
+            <span>Multan</span>
+        </div>
+
+        <div className="info">
+            <span>
+                <b>Works at</b>
+            </span>
+            <span>FLNG</span>
+        </div>
+
+        <button className='button'>Logout</button>
     </div>
   )
 }
