cd5a600fc3d13e1ba4990c31a865f967271d3281
MiguelAGuzman <miguel.a.guzman0009@gmail.com>
2022-11-12 23:06:03 -0500

    added post side and profilecard to profile page

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import ProfileLeft from '../../components/ProfileLeft/ProfileLeft'
+import ProfileCard from '../../components/ProfileCard/ProfileCard'
+import PostSide from '../../components/PostSide/PostSide'
 import './Profile.css'
 
 const Profile = () => {
   return (
     <div className="Profile">
         <ProfileLeft/>
+
+        <div className="ProfileCenter">
+          <ProfileCard/>
+          <PostSide/>
+        </div>
     </div>
   )
 }
