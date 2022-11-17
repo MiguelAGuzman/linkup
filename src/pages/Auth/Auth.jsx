3faa8520a2b4f0b513525bafd8dcb5d9c1fdb164
MiguelAGuzman <miguel.a.guzman0009@gmail.com>
2022-11-16 23:18:16 -0500

    added text and some styling to auth

--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -7,7 +7,13 @@ const Auth = () => {
     <div className="Auth">
         <div className="a-left">
             <img src={Logo} alt=""/>
+            <div className="WebName">
+                <h1>Linkup</h1>
+                <h6>Explore ideas throughout the world</h6>
+            </div>
         </div>
+
+        <h1>Form</h1>
     </div>
   )
 }
